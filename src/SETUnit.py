c2678be2a7bec35ae47bc4713ae54a0885b385c8
unknown <Rishi@.(none)>
2011-06-25 09:07:00 -0500

    Unit Testing for SET Game Funtionality Correctness

--- src/SETUnit.py
+++ src/SETUnit.py
@@ -1,19 +1,23 @@
 import unittest
 
+from Game import Game
+from Card import Card
+from GUIHandler import GUIHandler
 
 class  SETUnitTestCase(unittest.TestCase):
-    #def setUp(self):
-    #    self.foo = SETUnit()
-    #
+
+    def setUp(self):
+        self.testgame = Game()
+        self.testguihandler = GUIHandler(self.testgame)
+        self.testcard = Card(0,0,0,0)
 
     #def tearDown(self):
     #    self.foo.dispose()
     #    self.foo = None
 
-    def test_SETUnit(self):
-        #assert x != y;
-        #self.assertEqual(x, y, "Msg");
-        self.fail("TODO: Write test")
+    def testCardImageNumber(self):
+        self.assertEqual(self.testcard.getCardImgNumber(),0)
+
 
 if __name__ == '__main__':
     unittest.main()
